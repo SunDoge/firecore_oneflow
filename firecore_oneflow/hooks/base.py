7b255681ad2cc1d9f257e84ba0a030a509aef220
SunDoge <384813529@qq.com>
2023-03-14 10:39:49 +0000

    add baserunner

---
+++ firecore_oneflow/hooks/base.py
@@ -0,0 +1,22 @@
+class BaseHook:
+
+    def on_init(self, **kwargs):
+        pass
+
+    def before_epoch(self, **kwargs):
+        pass
+
+    def before_iter(self, **kwargs):
+        pass
+
+    def before_forward(self, **kwargs):
+        pass
+
+    def after_forward(self, **kwargs):
+        pass
+
+    def after_iter(self, **kwargs):
+        pass
+
+    def after_epoch(self, **kwargs):
+        pass
---
+++ firecore_oneflow/runners/base.py
@@ -0,0 +1,26 @@
+from firecore_oneflow.hooks.base import BaseHook
+from typing import List
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class BaseRunner:
+
+    def __init__(self, hooks: List[BaseHook], **kwargs) -> None:
+        self._kwargs = kwargs
+        self._hooks = hooks
+
+    def step(self, epoch: int):
+        pass
+
+    def call_hook(self, method: str, **kwargs):
+        logger.debug('call hook: %s', method)
+        for hook in self._hooks:
+            self.call_method(getattr(hook, method), **kwargs)
+
+    def call_method(self, func, **kwargs):
+        return func(**self.__dict__, **self._kwargs, **kwargs)
+
+    def register_hook(self, hook: BaseHook):
+        self._hooks.append(hook)
